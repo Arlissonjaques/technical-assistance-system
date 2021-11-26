be3c57ed339d20874fc5c75ce88465801ed086f8
Arlisson Jaques <arlissonjaques@gmail.com>
2021-11-26 20:24:29 -0300

    reformulates the validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ApplicationRecord
   belongs_to :address
 
-  validates :full_name, presence: true
-  validates :cpf, uniqueness: true, cpf: true
+  validates :cpf, cpf: true # this activates cpf validation
+  validates :full_name, :cpf, presence: true
+  validates_uniqueness_of :cpf, case_sensitive: false
 end
