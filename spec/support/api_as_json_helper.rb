0d06ded2e9ff2046e26a5e9c3ecfeacc2a871106
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-06 22:12:16 -0300

    add method #device_as_json

--- spec/support/api_as_json_helper.rb
+++ spec/support/api_as_json_helper.rb
@@ -21,4 +21,14 @@ module ApiAsJsonHelper
       'address_id' => client.address_id
     }
   end
+
+  def device_as_json(device)
+    {
+      'id' => device.id,
+      'name' => device.name,
+      'brand' => device.brand,
+      'model' => device.model,
+      'device_type_id' => device.device_type_id
+    }
+  end
 end
