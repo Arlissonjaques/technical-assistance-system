92f78914a264e542126b6a93729e1a61a3a6302d
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-06 22:09:34 -0300

    add exception for address actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     mount_devise_token_auth_for 'User', at: 'auth'
     namespace :v1 do
-      resources :addresses
+      resources :addresses, only: [:show, :create, :update]
       resources :clients
       resources :devices
     end
