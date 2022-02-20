a10e4772d5946aab1ec878acda499bf904fa0c3d
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-20 20:59:57 -0300

    add relation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :validatable
   include DeviseTokenAuth::Concerns::User
 
+  has_many :service_requisitions
+
   validates :name, :email, presence: true
   enum role: %i[employee manager admin]
 
